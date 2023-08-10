011e65a93e84397973fb27c675ebdff1b88265f3
jooooonj <lee22424205@naver.com>
2023-08-10 18:10:57 +0900

    feat: Post 엔티티 구현

---
+++ src/main/java/com/wanted/boardAPI/domain/post/entity/Post.java
@@ -0,0 +1,40 @@
+package com.wanted.boardAPI.domain.post.entity;
+
+import com.wanted.boardAPI.base.baseEntity.BaseTimeEntity;
+import com.wanted.boardAPI.domain.member.entity.Member;
+import com.wanted.boardAPI.domain.post.entity.request.CreatePostRequest;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    private Post(Member member, String title, String content){
+        this.member = member;
+        this.title = title;
+        this.content = content;
+    }
+
+    public static Post of(Member member, CreatePostRequest createPostRequest){
+        return new Post(member,
+                createPostRequest.getTitle(),
+                createPostRequest.getContent())
+        ;
+    }
+}
