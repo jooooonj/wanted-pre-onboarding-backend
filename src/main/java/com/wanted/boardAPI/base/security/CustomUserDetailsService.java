d9e1b337046ce9633ad49e36eab3e36baa70fda0
jooooonj <lee22424205@naver.com>
2023-08-10 14:36:41 +0900

    feat: 사용자 인증 처리를 위한 CustomUserDetailService 구현

---
+++ src/main/java/com/wanted/boardAPI/base/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.wanted.boardAPI.base.security;
+
+import com.wanted.boardAPI.domain.member.entity.Member;
+import com.wanted.boardAPI.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email+"로 가입된 계정이 없습니다."));
+        return new User(member.getEmail(), member.getPassword(), null);
+    }
+}
